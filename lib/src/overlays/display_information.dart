09ac10af4086a9cedbe021ed39d3135f10dd007a
Suebersson <suebersson.dev@gmail.com>
2023-08-17 00:26:37 -0300

    shared_widgets

--- lib/src/overlays/display_information.dart
+++ lib/src/overlays/display_information.dart
@@ -48,78 +48,78 @@ Future<void> displayInformation({
 
   return showGeneralDialog<void>(
     context: context,
-    barrierLabel: barrierLabel,
+    barrierLabel: barrierLabel ?? 'displayInformation',
     barrierDismissible: true,
     barrierColor: barrierColor ?? Colors.black45,
     transitionDuration: transitionDuration ?? const Duration(milliseconds: 400),
     pageBuilder: (context, animation, animation2){
       return Material(
-          color: Colors.transparent,
-          child: SafeArea(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: const ColoredBox(
-                    color: Colors.transparent,
-                    child: SizedBox(
-                      height: double.infinity,
-                      width: double.infinity,
-                    ),
-                  )
-                ),
-                Dismissible(
-                  onDismissed: (_) => Navigator.pop(context),
-                  key: const Key("displayInformationDismissible"),
-                  direction:  direction ?? DismissDirection.down,
-                  child: Container(
-                    height: height ?? 320.0,
+        color: Colors.transparent,
+        child: SafeArea(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: const ColoredBox(
+                  color: Colors.transparent,
+                  child: SizedBox(
+                    height: double.infinity,
                     width: double.infinity,
-                    alignment: Alignment.center,
-                    margin: margin ?? const EdgeInsets.symmetric(vertical: 2.0, horizontal: 20.0),
-                    padding: padding ?? const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: backgroundColor ?? Theme.of(context).dialogBackgroundColor,
-                      borderRadius: borderRadius ?? BorderRadius.circular(8.0),
-                    ),
-                    child: SingleChildScrollView(
-                      physics: backgroundPhysics ?? const NeverScrollableScrollPhysics(),
-                      scrollDirection: Axis.vertical,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            message,
-                            style: messageStyle,
-                            textAlign: TextAlign.left,
+                  ),
+                )
+              ),
+              Dismissible(
+                onDismissed: (_) => Navigator.pop(context),
+                key: const Key("displayInformationDismissible"),
+                direction:  direction ?? DismissDirection.down,
+                child: Container(
+                  height: height ?? 320.0,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  margin: margin ?? const EdgeInsets.symmetric(vertical: 2.0, horizontal: 20.0),
+                  padding: padding ?? const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: backgroundColor ?? Theme.of(context).dialogBackgroundColor,
+                    borderRadius: borderRadius ?? BorderRadius.circular(8.0),
+                  ),
+                  child: SingleChildScrollView(
+                    physics: backgroundPhysics ?? const NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          message,
+                          style: messageStyle,
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(height: 20.0),
+                        Visibility(
+                          visible: buttonOk != null,
+                          replacement: FilledButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'OK',
+                                style: messageStyle,
+                              ),
+                            )
                           ),
-                          const SizedBox(height: 20.0),
-                          Visibility(
-                            visible: buttonOk != null,
-                            replacement: FilledButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'OK',
-                                  style: messageStyle,
-                                ),
-                              )
-                            ),
-                            child: buttonOk ?? const SizedBox.shrink(),
-                          )
-                        ],
-                      )
-                    ),
+                          child: buttonOk ?? const SizedBox.shrink(),
+                        )
+                      ],
+                    )
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        );
+        ),
+      );
     },
     transitionBuilder: (context, animation, animation2, widget){
       return SlideTransition(
